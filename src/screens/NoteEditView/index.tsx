fad238d83900e3f54beef9041a35d9eb5e9029d5
Kota Nonaka <kota.kaicho@gmail.com>
2018-09-11 23:24:10 +0900

    save note function

--- src/screens/NoteEditView/index.tsx
+++ src/screens/NoteEditView/index.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import { connect } from 'react-redux';
 import Note from '../../model/note';
 import { RootState } from '../../module';
+import { saveNote } from '../../module/notes';
 
 interface PropTypes {
   notes: Note[],
@@ -9,28 +10,97 @@ interface PropTypes {
     params: {
       id: string
     }
-  }
+  },
+  dispatch: (action :any) => any
 }
 
-const NoteEditView: React.SFC<PropTypes> = (props) => {
-  const noteId = props.match.params.id;
-  const selectedNote = props.notes.find(note => note.id === noteId);
+interface StateTypes {
+  noteId: string,
+  draftTitle: string,
+  draftBody: string
+}
 
-  return (
-    <div>
-      {selectedNote ? (
-        <div>
-          <input type='text' value={selectedNote.title} />
-          <textarea value={selectedNote.body} />
-          <button>save</button>
-          <button>delete this note</button>
-        </div>
-      ) : (
-        <div>not found!</div>
-      )}
-    </div>
-  );
-};
+class NoteEditView extends React.Component<PropTypes, StateTypes> {
+  public constructor(props: PropTypes) {
+    super(props);
+
+    this.state = {
+      noteId: '',
+      draftTitle: '',
+      draftBody: ''
+    };
+  }
+
+  public componentDidMount() {
+    this.setCurrentNoteToState(this.props);
+  }
+
+  public componentWillReceiveProps(nextProps: PropTypes) {
+    this.setCurrentNoteToState(nextProps);
+  }
+
+  public render() {
+    const selectedNote = this.getSelectedNote();
+
+    return (
+      <div>
+        {selectedNote ? (
+          <div>
+            <input
+              type='text'
+              value={this.state.draftTitle}
+              onChange={this.onTitleChangeHandler}
+            />
+            <textarea
+              value={this.state.draftBody}
+              onChange={this.onBodyChangeHandler}
+            />
+            <button onClick={this.saveHandler}>save</button>
+            <button>delete this note</button>
+          </div>
+        ) : (
+          <div>not found!</div>
+        )}
+      </div>
+    );
+  }
+
+  private saveHandler = () => {
+    this.props.dispatch(saveNote(this.state.noteId, this.state.draftTitle, this.state.draftBody));
+  }
+
+  private onTitleChangeHandler = (event: React.FormEvent<HTMLInputElement>) => {
+    const target = event.target as HTMLInputElement;
+    this.setState({
+      draftTitle: target.value
+    });
+  }
+
+  private onBodyChangeHandler = (event: React.FormEvent<HTMLTextAreaElement>) => {
+    const target = event.target as HTMLInputElement;
+    this.setState({
+      draftBody: target.value
+    });
+  }
+
+  private getSelectedNote(props: PropTypes = this.props) {
+    const noteId = props.match.params.id;
+    return props.notes.find(note => note.id === noteId);
+  }
+
+  private setCurrentNoteToState(props: PropTypes) {
+    const selectedNote = this.getSelectedNote(props);
+
+    if (selectedNote) {
+      this.setState({
+        noteId: selectedNote.id,
+        draftTitle: selectedNote.title,
+        draftBody: selectedNote.body
+      });
+    }
+  }
+
+}
 
 const mapStateToProps = (state: RootState) => {
   return {
